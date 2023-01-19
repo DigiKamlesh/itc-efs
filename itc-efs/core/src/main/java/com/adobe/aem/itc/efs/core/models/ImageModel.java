888d7e88d6bfee0f1aef866015d6922b61682b97
DESKTOP-PLNTQ83\nikhi <nikhilt@digiquadsolkutions.com>
2023-01-19 15:12:40 +0530

    Hero component

---
+++ itc-efs/core/src/main/java/com/adobe/aem/itc/efs/core/models/ImageModel.java
@@ -0,0 +1,22 @@
+package com.adobe.aem.itc.efs.core.models;
+
+public interface ImageModel {
+
+	   String getTitle();
+
+	    String getDescription();
+
+	    String getImage();
+
+	    String getAltText();
+
+	    String getMobileImage();
+	    
+	    String getButton1();
+	    
+	    String getLinkURL1();
+	    
+	    String buttontextOne();
+
+	    boolean isImageEmpty();
+}
